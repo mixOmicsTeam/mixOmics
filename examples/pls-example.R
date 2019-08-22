e731e4527c569076b0a50661d3f74096e3e48252
Al J Abadi <al.jal.abadi@gmail.com>
2019-08-22 12:47:00 +1000

    add and test pls examples

--- examples/pls-example.R
+++ examples/pls-example.R
@@ -1,6 +1,6 @@
 #' \dontrun{
+library(mixOmics.data)
 data(linnerud)
-
 ## ---------------- with X and Y as matrices
 X <- linnerud$exercise
 Y <- linnerud$physiological
@@ -16,16 +16,19 @@ identical(pls.res1[-1], pls.res2[-1])
 #> TRUE
 ## ---------------- MultiAssayExperiment and assay names as X and Y
 ## 'data' argument should be explicitly mentioned for correct method dispatch
-pls.res3 <- pls(X='exercise', Y='physiological', data = linnerud.mae)
+data("linnerud.mae")
+pls.res3 <- pls(X='exercise', Y='physiological', data = linnerud.mae, mode = "regression")
 identical(pls.res1[-1], pls.res3[-1])
 #> TRUE
 
 ## ---------------- MultiAssayExperiment and formula with assay names
-pls.res4 <- pls(formula = physiological ~ exercise, data = linnerud.mae, mode = "classic",)
+pls.res4 <- pls(formula = physiological ~ exercise, data = linnerud.mae)
 identical(pls.res1[-1], pls.res4[-1])
 #> TRUE
 
-## ---------------- MultiAssayExperiment; X=assay and Y=colData
+## ---------------- data=MultiAssayExperiment; X=assay and Y=colData
+data("liver.toxicity")
+data("liver.toxicity.mae")
 toxicity.pls1 <- pls(data = liver.toxicity.mae,  formula = Dose.Group~gene, ncomp = 3)
 toxicity.pls2 <- pls(data = liver.toxicity.mae,  Y='Dose.Group', X='gene', ncomp = 3)
 identical(toxicity.pls1[-1], toxicity.pls2[-1])
