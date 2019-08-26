a5d4fc17037394da9be374c151858ade488c0979
Al J Abadi <al.jal.abadi@gmail.com>
2019-08-26 11:52:44 +1000

    add test-block.pls helper

--- tests/testthat/helper-block.pls.R
+++ tests/testthat/helper-block.pls.R
@@ -1,12 +1,12 @@
 library(mixOmicsData)
 library(MultiAssayExperiment)
-
+data(breast.TCGA.mae)
 ## This is a parametrised test set for combinations of X, Y, formula, and daya in pls
 ## the tests should satisfy the conditions explained in documentation of pls function.
 
 ## -------------- parameters to change --------------
 ## MAE data
-block.mae <- breast.TCGA.mae
+data_bpls <- breast.TCGA.mae
 
 ## index of the X assays in MAE data
 X_indices <- 1:2 ## mrna, mirna
@@ -18,9 +18,14 @@ Yc_index <- 1 ## "subtype" - must be numeric
 ## -------------- no need to change the following --------------
 ## the names used down here must be unique across all helper-* files
 
-X_bpls <- as.list(experiments(block.mae)[X_indices])
-Y_bpls <- experiments(block.mae)[[Y_index]]
-
+Xa_bpls <- names(experiments(data_bpls))[X_indices] ## X assays
+Ya_bpls <-  names(experiments(data_bpls))[Y_index] ## Y assay
+## formula build
+formula_bpls <- sprintf(fmt = "%s ~ %s", Ya_bpls, 
+                        paste(Xa_bpls, collapse = " + "))
+## list and matrix inputs
+X_bpls <- as.list(experiments(data_bpls)[X_indices])
+Y_bpls <- experiments(data_bpls)[[Y_index]]
 ## transpose
 X_bpls <- lapply(X_bpls, t)
 Y_bpls <- t(Y_bpls)
