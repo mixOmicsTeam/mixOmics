c9996171af987ed05973db1a0fb1d7b0495bb609
Al J Abadi <al.jal.abadi@gmail.com>
2019-08-27 18:31:15 +1000

    block.plsda 'ANY'
    Need to get Y from colData

--- R/utils_plsda.R
+++ R/utils_plsda.R
@@ -16,6 +16,7 @@
     
     Y.mat = unmap(mc$Y)
     colnames(Y.mat) = levels(mc$Y)
+    rownames(Y.mat) <- rownames(mc$X)
     mc$Y <- Y.mat
     
   } else {
@@ -33,5 +34,51 @@
     mc$multilevel = data.frame(mc$multilevel, mc$Y)
     mc$multilevel[, 1] = as.numeric(factor(mc$multilevel[, 1])) # we want numbers for the repeated measurements
   }
+  mc
+}
+
+.check_plsda_block <- function(mc) {
+  Y <- mc$Y
+  X <- mc$X
+  indY <- mc$indY
+  
+  if (!isNULL(Y))
+  {
+    if (is.null(dim(Y)))
+    {
+      Y = factor(Y)
+    } else {
+      stop("'Y' should be a factor or a class vector.")
+    }
+    
+    if (nlevels(Y) == 1)
+      stop("'Y' should be a factor with more than one level")
+    
+    Y.input = Y
+    Y = unmap(Y)
+    colnames(Y) = levels(Y.input)
+    rownames(Y) = rownames(X[[1]])
+  } else if (!isNULL(indY)) {
+    temp = X[[indY]]
+    #not called Y to not be an input of the wrapper.sparse.mint.block
+    if (is.null(dim(temp)))
+    {
+      temp = factor(temp)
+    } else {
+      stop("'Y' should be a factor or a class vector.")
+    }
+    
+    if (nlevels(temp) == 1)
+      stop("'X[[indY]]' should be a factor with more than one level")
+    
+    Y.input = temp
+    X[[indY]] = unmap(temp)
+    colnames(X[[indY]]) = levels(Y.input)
+    rownames(X[[indY]]) = rownames(X[[ifelse(indY==1,2,1)]])
+    
+  } else if (isNULL(indY)) {
+    stop("Either 'Y' or 'indY' is needed")
+  }
+  mc[c("X", "Y", "indY")] <- list(X, Y, indY)
   mc
 }
