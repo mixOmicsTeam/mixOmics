b9820fcb7f295e9ca9f32e4833e4e90b9b50892f
Al J Abadi <al.jal.abadi@gmail.com>
2019-08-28 12:27:59 +1000

    move examples to ./examples

--- examples/block.plsda-example.R
+++ examples/block.plsda-example.R
@@ -3,15 +3,15 @@ data = list(gene = nutrimouse$gene, lipid = nutrimouse$lipid, Y = nutrimouse$die
 # with this design, all blocks are connected
 design = matrix(c(0,1,1,1,0,1,1,1,0), ncol = 3, nrow = 3,
 byrow = TRUE, dimnames = list(names(data), names(data)))
-
+## when index of Y in list of X is provided
 res = block.plsda(X = data, indY = 3) # indY indicates where the outcome Y is in the list X
 plotIndiv(res, ind.names = FALSE, legend = TRUE)
 plotVar(res)
 
 
 # when Y is provided
-res2 = block.plsda(list(gene = nutrimouse$gene, lipid = nutrimouse$lipid),
+res = block.plsda(X = list(gene = nutrimouse$gene, lipid = nutrimouse$lipid),
 Y = nutrimouse$diet, ncomp = 2)
-plotIndiv(res2)
-plotVar(res2)
+plotIndiv(res)
+plotVar(res)
 #' }
