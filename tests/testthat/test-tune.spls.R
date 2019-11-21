432df24f24788f58892425cf290170b1db4a122b
Al J Abadi <al.jal.abadi@gmail.com>
2019-11-21 11:28:13 +1100

    improve tune.spls tests

--- tests/testthat/test-tune.spls.R
+++ tests/testthat/test-tune.spls.R
@@ -8,12 +8,16 @@ test_that("tune.spls works with and without parallel", code = {
     ncomp <- 2
     nrepeat <- 3
     
+    RNGversion("3.5.0") ## in case RNG changes!
+    RNGkind("L'Ecuyer-CMRG") ## works for parallel as well
+    
+    set.seed(100)
     tune.spls11 <- tune.spls(X, Y, ncomp = ncomp, test.keepX = test.keepX,
                               nrepeat = nrepeat)
     expect_is(tune.spls11, "tune.spls")
-
     
+    set.seed(100)
     tune.spls31 <- tune.spls(X, Y, ncomp = ncomp, test.keepX = test.keepX,
-                             nrepeat = nrepeat, cpus = 3)
-    expect_is(tune.spls31, "tune.spls")
+                             nrepeat = nrepeat, cpus = 2)
+    expect_identical2(tune.spls11, tune.spls31)
 })
