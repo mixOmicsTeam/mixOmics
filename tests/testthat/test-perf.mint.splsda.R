265b7b9fd0233d67de86d363510a1f743426e76a
Al J Abadi <al.jal.abadi@gmail.com>
2020-01-28 12:11:41 +1100

    remove duplicate test

--- tests/testthat/test-perf.mint.splsda.R
+++ tests/testthat/test-perf.mint.splsda.R
@@ -32,25 +32,3 @@ test_that("perf.mint.splsda works with custom alpha", code = {
     expect_true(all(out$choice.ncomp == 1))
     
 })
-test_that("tune.mint.splsda works with custom alpha", code = {
-    data(stemcells)
-    data = stemcells$gene
-    type.id = stemcells$celltype
-    exp = stemcells$study
-    
-    out = tune.mint.splsda(
-        X = data,
-        Y = type.id,
-        ncomp = 2,
-        near.zero.var = FALSE,
-        study = exp,
-        test.keepX = seq(1, 5, 2),
-        signif.threshold = 0.1
-    )
-    
-    out$choice.ncomp
-    out$choice.keepX
-    expect_is(out, "tune.mint.splsda")
-    expect_equal(out$choice.ncomp$ncomp, 1)
-    
-})
