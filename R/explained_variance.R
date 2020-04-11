5880703e521b38a07e361ce90e45f23cbf872dc7
Al J Abadi <al.jal.abadi@gmail.com>
2020-04-11 23:02:52 +1000

    roxygenated: explained_variance

--- R/explained_variance.R
+++ R/explained_variance.R
@@ -1,33 +1,39 @@
-################################################################################
-# Authors:
-#   Florian Rohart,
-#   Kim-Anh Le Cao,
-#
-# created: 15-04-2015
-# last modified: 04-10-2017
-#
-# Copyright (C) 2015
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-###############################################################################
-
 # =============================================================================
 # Calculate the explained variance of one dataset based on its variates
 # =============================================================================
 
-
+#' Calculation of explained variance
+#' 
+#' This function calculates the variance explained by variates.
+#' 
+#' 
+#' \code{explained_variance} calculates the explained variance of each variates
+#' out of the total variance in \code{data}.
+#' 
+#' @param data numeric matrix of predictors
+#' @param variates variates as obtained from a \code{pls} object for instance
+#' @param ncomp number of components. Should be lower than the number of
+#' columns of \code{variates}
+#' @return \code{explained_variance} simply returns the explained variance for
+#' each variate.
+#' @author Florian Rohart, Kim-Anh Lê Cao, Al J Abadi
+#' @seealso \code{\link{spls}}, \code{\link{splsda}}, \code{\link{plotIndiv}},
+#' \code{\link{plotVar}}, \code{\link{cim}}, \code{\link{network}}.
+#' @keywords regression multivariate
+#' @examples
+#' 
+#' data(liver.toxicity)
+#' X <- liver.toxicity$gene
+#' Y <- liver.toxicity$clinic
+#' 
+#' toxicity.spls <- spls(X, Y, ncomp = 2, keepX = c(50, 50), keepY = c(10, 10))
+#' 
+#' ex = explained_variance(toxicity.spls$X, toxicity.spls$variates$X, ncomp =2)
+#' 
+#' # ex should be the same as
+#' toxicity.spls$explained_variance$X
+#' 
+#' @export
 explained_variance = function(data, variates, ncomp)
 {
     #check input data
