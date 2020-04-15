45b92adb14cd84d399272792225afdbdae75bbb4
Al J Abadi <al.jal.abadi@gmail.com>
2020-04-15 15:40:18 +1000

    add tune.spca
    Working example:
    
    data(multidrug)
     tune.spca_res <- tune.spca(X = multidrug$ABC.trans, comp = 2, nrepeat = 3, kfold = 3, grid.keepX = seq(5,35,5))
     tune.spca_res
    #>      result.1 result.2
    #> [1,]       20       20

---
+++ R/tune.spca.R
@@ -0,0 +1,79 @@
+tune.spca <- function(X, comp, nrepeat, kfold, grid.keepX) {
+
+    keepX.dim <- NULL
+    result = foreach(comp = as.list(1:comp),.combine=cbind) %do% {
+        
+        # 1 - a foreach list for each keepX value tested
+        cor.pred.repeat.keepX.dim = foreach(keepX.value = as.list(grid.keepX),.combine=cbind) %do% {
+            cat('KeepX = ', keepX.value, '\n')  # to remove in the final function
+            
+            # 2 - a foreach list for repeated CV
+            cor.pred.repeat = foreach(i = as.list(c(1:nrepeat)),.combine=cbind) %do% {
+                folds = split(sample(1:nrow(X)),1:kfold)
+                
+                # 3 -  a foreach list for k-fold CV
+                #  if small n, then need to define the fold based on boostrapping (with replacement) as we need to center / scale the data for prediction
+                cor.pred = foreach(i = folds,.combine=cbind) %do% {
+                    # determine matrix without the fold and with the fold
+                    X.minus.sub = X[-i,]  # could rename as train
+                    X.sub = X[i,]  # used for prediction, could rename as test
+                    X.sub.scale = scale(X.sub, center = TRUE, scale = TRUE) # used for deflation
+                    
+                    # ---- run sPCA ------------ #
+                    if(comp ==1){ # for comp = 1
+                        # spca on the data minus the subsample
+                        spca.res.sub = mixOmics::spca(X.minus.sub, ncomp = comp, keepX = keepX.value, center = TRUE, scale = TRUE)
+                        # spca on all data 
+                        spca.res.full = mixOmics::spca(X, ncomp = comp, keepX = keepX.value, center = TRUE, scale = TRUE)
+                    }else{ # comp >1
+                        # spca on the data minus the subsample
+                        spca.res.sub = mixOmics::spca(X.minus.sub, ncomp = comp, keepX = c(keepX.dim, keepX.value), center = TRUE, scale = TRUE)
+                        # spca on all data 
+                        spca.res.full = mixOmics::spca(X, ncomp = comp, keepX = c(keepX.dim, keepX.value), center = TRUE, scale = TRUE)
+                    }
+                    
+                    # ---- deflation on X with only the fold left out------------ #
+                    for(k in 1:comp){ # loop to calculate deflated matrix and predicted comp
+                        # calculate the predicted comp on the fold left out
+                        # calculate reg coeff, then deflate
+                        if(k == 1){
+                            t.comp.sub = X.sub.scale %*% spca.res.sub$loadings$X[,k]
+                        }else{ # calculate deflation beyond comp 1
+                            # recalculate the loading vector (here c.sub) on the test set (perhaps we could do this instead on the training set by extracting from spca.res.sub$loadings$X[,k]?)
+                            c.sub = crossprod(X.sub.scale, t.comp.sub) / drop(crossprod(t.comp.sub)) 
+                            X.sub.scale= X.sub.scale - t.comp.sub %*% t(c.sub) 
+                            # update predicted comp based on deflated matrix
+                            t.comp.sub = X.sub.scale %*% spca.res.sub$loadings$X[,k]
+                        }
+                    }
+                    
+                    # ---- calculate predicted component and compare with component from sPCA on full data on the left out set------------ #
+                    # cor with the component on the full data, abs value
+                    cor.comp = abs(cor(t.comp.sub, spca.res.full$variates$X[,comp][i], use = 'pairwise.complete.obs'))
+                    # need to flip the sign in one of the comp to calculate the RSS
+                    #RSS.comp = sum((c(sign(cor.comp))*(t.comp.sub) - spca.res.full$variates$X[,comp][i])^2)
+                    # also look at correlation between loading vectors
+                    #cor.loading = abs(cor(spca.res.full$loadings$X[,comp], spca.res.sub$loadings$X[,comp]))
+                    
+                    # return
+                    return(cor.comp)  #, cor.loading, RSS.comp))
+                }  # end foreach 3 (on folds), get the cor(pred component, comp) per fold for a given comp
+                
+                # output is correlation between reconstructed components cor.comp on the left out samples and if we had those samples in PCA, as well as correlation between loading vectors cor.loading just to check both trends
+                rownames(cor.pred) = c('cor.comp')  #, 'cor.loading', 'RSS.comp')
+                # average correlations across folds
+                return(abs(apply(cor.pred, 1, mean)))
+            } # end foreach 2 (on repeats), get the cor(pred component, comp) averaged across folds for a given comp
+            
+            # average correlation across repeats 
+            return(abs(apply(cor.pred.repeat, 1, mean)))
+        } # end foreach 1 (on keepX), get the cor(pred component, comp) averaged across repeats for a given comp and each keepX
+        
+        # # correlation for each keepX value
+        cor.pred.per.dim[[comp]]  = cor.pred.repeat.keepX.dim
+        
+        # working out the max based on cor.comp and append to keepX.dim
+        keepX.dim = c(keepX.dim, grid.keepX[which.max(cor.pred.repeat.keepX.dim['cor.comp',])])
+        return(keepX.dim)  # I think this output is irrelevant, as it is combining across comp per column (consider the last column)
+    }
+}
