8d9ea80ff7d079659bc235b05c08dbc31fe53b1b
Al J Abadi <al.jal.abadi@gmail.com>
2020-06-29 13:08:32 +1000

    reset graphics for plotvar test

--- tests/testthat/test-plotVar.R
+++ tests/testthat/test-plotVar.R
@@ -8,7 +8,8 @@ test_that("plotVar works for pls with var.names", {
   var.names <- list(x = seq_along(x), y = seq_along(y))
   
   pls.res <- pls(x, y) 
-  df <- plotVar(pls.res , var.names = var.names)
+  dev.off()
+  df <- invisible(plotVar(pls.res , var.names = var.names))
   
   var.names.char.vec <- unname(unlist(lapply(var.names, as.character)))
   
