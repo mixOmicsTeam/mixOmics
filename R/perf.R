eb01dfa87a54df145038fd7403cb6cfea3716fa7
Al J Abadi <al.jal.abadi@gmail.com>
2020-10-20 14:07:56 +1100

    take perf.mixo_pls6 and perf_pls_repeat into perf.mixo_pls
    which takes 'nrepeat' arg now

--- R/perf.R
+++ R/perf.R
@@ -238,12 +238,13 @@ perf <- function(object, ...)
 ## -------------------------------- (s)PLS -------------------------------- ##
 #' @rdname perf
 #' @export
-perf.mixo_pls <- function(object,
+.perf.mixo_pls_folds <- function(object,
                           validation = c("Mfold", "loo"),
                           folds = 10,
                           progressBar = FALSE,
                           ...)
 {
+    # TODO proper progressBar
     # changes to bypass the loop for the Q2
 
     ## -------- checks -------- ##
@@ -595,6 +596,78 @@ perf.mixo_pls <- function(object,
     return(invisible(result))
 }
 
+perf.mixo_pls <- function(object,
+                          validation = c("Mfold", "loo"),
+                          folds = 10,
+                          progressBar = FALSE,
+                          nrepeat = 1,
+                          ...)
+{
+    
+    
+    #-- initialising arguments --#
+    # these are the centered and scaled matrices output from pls, we remove $nzv if needed
+    if (length(object$nzv$Position)>0)
+    {
+        X = object$X[, -object$nzv$Position]
+    } else {
+        X = object$X
+    }
+    Y = object$Y
+    
+    scale = object$scale
+    tol = object$tol
+    max.iter = object$max.iter
+    mode = object$mode
+    ncomp = object$ncomp
+    n = nrow(X)
+    p = ncol(X)
+    q = ncol(Y)
+    
+    keepX = object$keepX
+    keepY = object$keepY
+    
+    
+    out = list()
+    
+    measure = NULL
+    cor.tpred = cor.upred = RSS.tpred = RSS.upred = matrix(nrow = ncomp, ncol = nrepeat, 
+                                                           dimnames = list(paste0('comp', 1:ncomp), paste0('repeat', 1:nrepeat)))
+    Q2.tot.ave = matrix(nrow = ncomp, ncol = nrepeat,
+                        dimnames = list(paste0('comp', 1:ncomp), paste0('repeat', 1:nrepeat)))
+    
+    cor.pred = RSS.pred = list()
+    
+    for(k in 1:nrepeat){
+        cat('repeat', k, '\n')
+        # fold CV
+        res.perf = .perf.mixo_pls_folds(object, validation = validation, folds = folds)
+        
+        # extract Q2.total, we could extract other outputs such as R2, MSEP etc (only valid for regression)
+        Q2.tot.ave[, k] = res.perf$Q2.total 
+        
+        # extract the predicted components per dimension, take abs value
+        cor.tpred[, k] = diag(abs(cor(res.perf$t.pred.cv, object$variates$X)))
+        cor.upred[, k] = diag(abs(cor(res.perf$u.pred.cv, object$variates$Y)))
+        
+        # RSS: no abs values here
+        RSS.tpred[, k] = apply((res.perf$t.pred.cv - object$variates$X)^2, 2, sum)/(nrow(X) -1)
+        RSS.upred[, k] = apply((res.perf$u.pred.cv - object$variates$Y)^2, 2, sum)/(nrow(X) -1)
+    } #end repeat       
+    
+    # # calculate mean across repeats
+    cor.pred$u = apply(cor.upred, 1, mean) 
+    cor.pred$t = apply(cor.tpred, 1, mean)
+    RSS.pred$u = apply(RSS.upred, 1, mean)
+    RSS.pred$t = apply(RSS.tpred, 1, mean)
+    
+    out$cor.pred = cor.pred
+    out$RSS.pred = RSS.pred
+    out$Q2.tot.ave = apply(Q2.tot.ave, 1, mean)
+    return(out)
+    
+}
+    
 #' @rdname perf
 #' @export
 perf.mixo_spls  <- perf.mixo_pls
