d4b5b71934a4bb4c1301597830e6337b221a224a
Al J Abadi <al.jal.abadi@gmail.com>
2020-11-13 13:25:44 +1100

    pca: update examples

--- examples/pca-examples.R
+++ examples/pca-examples.R
@@ -1,31 +1,36 @@
 # example with missing values where NIPALS is applied
 # --------------------------------
 data(multidrug)
-pca.res <- pca(multidrug$ABC.trans, ncomp = 4, scale = TRUE)
+X <- multidrug$ABC.trans
+pca.res <- pca(X, ncomp = 4, scale = TRUE)
 plot(pca.res)
 print(pca.res)
 biplot(pca.res, group = multidrug$cell.line$Class, legend.title = 'Class')
 
 # samples representation
 plotIndiv(pca.res, ind.names = multidrug$cell.line$Class,
-group = as.numeric(as.factor(multidrug$cell.line$Class)))
+    group = as.numeric(as.factor(multidrug$cell.line$Class)))
 
 # variable representation
-plotVar(pca.res, cutoff = 0.7, pch = 16)
+plotVar(pca.res, var.names = TRUE, cutoff = 0.4, pch = 16)
 
 \dontrun{
 plotIndiv(pca.res, cex = 0.2,
-col = as.numeric(as.factor(multidrug$cell.line$Class)),style="3d")
+    col = as.numeric(as.factor(multidrug$cell.line$Class)),style="3d")
 
 
-plotVar(pca.res, rad.in = 0.5, cex = 0.5,style="3d")
+plotVar(pca.res, rad.in = 0.5, cex = 0.5, style="3d")
 }
+
 # example with multilevel decomposition and CLR log ratio transformation (ILR longer to run)
 # ----------------
 
 data("diverse.16S")
 pca.res = pca(X = diverse.16S$data.TSS, ncomp = 3,
-logratio = 'CLR', multilevel = diverse.16S$sample)
+    logratio = 'CLR', multilevel = diverse.16S$sample)
 plot(pca.res)
-plotIndiv(pca.res, ind.names = FALSE, group = diverse.16S$bodysite, title = '16S diverse data',
-legend = TRUE, legend.title = 'Bodysite')
+plotIndiv(pca.res, ind.names = FALSE, 
+          group = diverse.16S$bodysite, 
+          title = '16S diverse data',
+          legend = TRUE, 
+          legend.title = 'Bodysite')
