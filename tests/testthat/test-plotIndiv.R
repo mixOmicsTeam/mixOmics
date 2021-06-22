0876f9807109c304d5dad0203a2774a8d9e762ad
Al J Abadi <al.jal.abadi@gmail.com>
2021-06-22 10:00:31 +1000

    fix bioc build

--- tests/testthat/test-plotIndiv.R
+++ tests/testthat/test-plotIndiv.R
@@ -107,10 +107,7 @@ test_that("plotIndiv.rcc works without ind.names", code = {
     Y <- nutrimouse$gene
     nutri.res <- rcc(X, Y, ncomp = 3, lambda1 = 0.064, lambda2 = 0.008)
     
-    plotIndiv.res <- tryCatch(expr = {plotIndiv(nutri.res, group = nutrimouse$genotype, ind.names = FALSE, legend = TRUE)},
-                              error = function(e) e,
-                              warning = function(w) w
-    )
+    plotIndiv.res <- plotIndiv(nutri.res, group = nutrimouse$genotype, ind.names = FALSE, legend = TRUE)
     
     expect_is(plotIndiv.res$graph, "ggplot")
 })
