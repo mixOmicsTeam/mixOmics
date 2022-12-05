826a801e90f8bee0421ee6c163ca57042a4a9b06
Max-Bladen <mbladen19@gmail.com>
2022-12-06 10:44:08 +1100

    Fix for Issue #275
    test: added new test file for `tune.spca()`

---
+++ tests/testthat/test-tune.spca.R
@@ -0,0 +1,43 @@
+
+test_that("tune.spca works", {
+  
+  set.seed(5212)
+  
+  data(srbct)
+  X <- srbct$gene[1:20, 1:200]
+  
+  grid.keepX <- seq(5, 35, 10)
+  
+  object <- tune.spca(X,ncomp = 2, 
+                      folds = 5, 
+                      test.keepX = grid.keepX, nrepeat = 3)
+  
+  expect_equal(object$choice.keepX[[1]], 35)
+  expect_equal(object$choice.keepX[[2]], 5)
+})
+
+test_that("tune.spca works with NA input", {
+  
+  set.seed(5212)
+  
+  data(srbct)
+  X <- srbct$gene[1:20, 1:200]
+  
+  na.feats <- sample(1:ncol(X), 20)
+  
+  for (c in na.feats) {
+    na.samples <- sample(1:nrow(X),1) #  sample.int(3, 1)
+    
+    X[na.samples, c] <- NA
+  }
+  
+  grid.keepX <- seq(5, 35, 10)
+  
+  expect_warning({object <- tune.spca(X,ncomp = 2, 
+                      folds = 5, 
+                      test.keepX = grid.keepX, nrepeat = 3)},
+                 "NAs present")
+  
+  expect_equal(object$choice.keepX[[1]], 15)
+  expect_equal(object$choice.keepX[[2]], 5)
+})
