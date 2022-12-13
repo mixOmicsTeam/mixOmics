ba154b5abb7813f8acaa4cdc8128a545ec02f385
Max Bladen <60872845+Max-Bladen@users.noreply.github.com>
2022-12-14 10:13:24 +1100

    PR addressing Issue #214 (#218)
    fix: implemented BiocParallel functionality into `tune.spls()` via restructuring of the main portion of the function. bplapply() iterates over all pairs of input keepX/Y so only reduces runtime for long lists of test.keepX/Y
    
    refactor: previous PR left duplicate function used for development. this is rectified and more comments are added
    
    test: added test cases for `tune.spls()`. These are just placeholders as more robust testing criteria needs to be established as well as this document will be fully updated as part of PR #206
    
    tests: updated tests to assess actual values alongside RNG control of parallel function calls.
    
    refactor: replaced all calls to `cpus` to `BPPARAM` in `tune()`
    
    docs: updated `tune.Rd` file with cpus -> BPPARAM shift

--- tests/testthat/test-tune.spls.R
+++ tests/testthat/test-tune.spls.R
@@ -1,59 +1,57 @@
-context("tune.spls")
-
-test_that("tune.spls works with and without parallel", code = {
+test_that("tune.spls works ", code = {
     data("nutrimouse")
     X <- nutrimouse$gene
     Y <- nutrimouse$lipid
-    test.keepX <- c(5,10)
-    ncomp <- 2
-    nrepeat <- 3
-    folds <- 3
     
-    RNGversion(.mixo_rng()) ## in case RNG changes!
+    set.seed(42)
+    tune.spls.res = suppressWarnings(tune.spls(X, Y, ncomp = 3,
+                              test.keepX = seq(5, 10, 5),
+                              test.keepY = seq(3, 6, 3), measure = "cor",
+                              folds = 5, nrepeat = 3, progressBar = F,
+                              BPPARAM = SerialParam(RNGseed = 5212)))
     
-    ## ----------- tune.spls works fine ----------- 
-    set.seed(12)
-    tune.spls11 <-
-        tune.spls(
-            X = X,
-            Y = Y,
-            ncomp = ncomp,
-            measure = 'cor',
-            test.keepX = test.keepX,
-            test.keepY = test.keepX,
-            folds = folds,
-            nrepeat = nrepeat
-        )
-    expect_is(tune.spls11, "tune.spls")
-    expect_equal(c(comp1 = 10, comp2 = 5), tune.spls11$choice.keepX)
-    expect_equal(c(comp1 = 5, comp2 = 5), tune.spls11$choice.keepY)
+    expect_equal(class(tune.spls.res), c("tune.pls", "tune.spls"))
+    expect_equal(unname(tune.spls.res$choice.keepX), c(5,5,5))
+    expect_equal(unname(tune.spls.res$choice.keepY), c(3,3,6))
+})
+
+test_that("tune.spls works in parallel", code = {
     
-    ## ----------- tune(method="spls") same as tune.spls ----------- 
-    set.seed(12)
-    tune.spls12 <-
-        tune(
-            method = "spls",
-            X = X,
-            Y = Y,
-            ncomp = ncomp,
-            measure = 'cor',
-            test.keepX = test.keepX,
-            test.keepY = test.keepX,
-            folds = folds,
-            nrepeat = nrepeat
-        )
+    library(BiocParallel)
+    data("nutrimouse")
+    X <- nutrimouse$gene
+    Y <- nutrimouse$lipid
     
-    .almost_identical(tune.spls11, tune.spls12)
+    set.seed(42)
+    tune.spls.res = suppressWarnings(tune.spls(X, Y, ncomp = 3,
+                                      test.keepX = seq(1, 5, 1),
+                                      test.keepY = seq(3, 6, 3), measure = "cor",
+                                      folds = 5, nrepeat = 3, progressBar = F,
+                                      BPPARAM = SnowParam(RNGseed = 5212, workers = 4)))
     
-    ## ----------- tune.spls parallel works fine ----------- 
-    # set.seed(12)
-    # tune.spls31 <- tune.spls(X, Y, ncomp = ncomp, test.keepX = test.keepX, folds = folds,
-    #                          nrepeat = nrepeat, BPPARAM = SnowParam(workers = 2, RNGseed = 12))
-                             # TODO tune.spls runs full model nrepeat times for each keepX and keepY!
-                             # TODO tune.spls fails with SnowParam
-    # expect_is(tune.spls31, "tune.spls")
-    # set.seed(12)
-    # tune.spls13 <- tune(method = "spls", X = X, Y = Y, ncomp = ncomp, test.keepX = test.keepX, folds = folds,
-    #                     nrepeat = nrepeat, cpus = 2)
-    # .almost_identical(tune.spls13, tune.spls31)
+    expect_equal(class(tune.spls.res), c("tune.pls", "tune.spls"))
+    expect_equal(unname(tune.spls.res$choice.keepX), c(1,1,1))
+    expect_equal(unname(tune.spls.res$choice.keepY), c(3,3,3))
+})
+
+test_that("tune.spls and tune(method='spls') are equivalent", {
+    
+    data("nutrimouse")
+    X <- nutrimouse$gene
+    Y <- nutrimouse$lipid
+    
+    set.seed(42)
+    tune.spls.res.1 = suppressWarnings(tune.spls(X, Y, ncomp = 3,
+                                      test.keepX = seq(1, 2, 1),
+                                      test.keepY = seq(3, 6, 3),
+                                      folds = 5, nrepeat = 3, progressBar = F,
+                                      BPPARAM = SerialParam(RNGseed = 5212)))
+    
+    tune.spls.res.2 = suppressWarnings(tune(method = "spls", X, Y, ncomp = 3,
+                                      test.keepX = seq(1, 2, 1),
+                                      test.keepY = seq(3, 6, 3),
+                                      folds = 5, nrepeat = 3, progressBar = F,
+                                      BPPARAM = SerialParam(RNGseed = 5212)))
+    
+    expect_equal(tune.spls.res.1$measure.pred, tune.spls.res.2$measure.pred)
 })
