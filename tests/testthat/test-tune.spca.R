ad08417ae6d6c716422916fa556d6e0103cefcbe
Max-Bladen <mbladen19@gmail.com>
2022-12-13 11:04:22 +1100

    Fix for Issue #279
    tests: included `RNGseed` within a `SerialParam()` call for `tune.spca()`.

--- tests/testthat/test-tune.spca.R
+++ tests/testthat/test-tune.spca.R
@@ -7,13 +7,14 @@ test_that("tune.spca works", {
   
   grid.keepX <- seq(5, 35, 10)
   
-  RNGversion(.mixo_rng())
   set.seed(5212)
   object <- tune.spca(X,ncomp = 2, 
                       folds = 5, 
-                      test.keepX = grid.keepX, nrepeat = 3)
+                      test.keepX = grid.keepX, nrepeat = 3,
+                      BPPARAM = SerialParam(RNGseed = 5212))
   
   expect_equal(object$choice.keepX[[1]], 35)
+  expect_equal(object$choice.keepX[[2]], 5)
 })
 
 
@@ -22,6 +23,7 @@ test_that("tune.spca works with NA input", {
   data(srbct)
   X <- srbct$gene[1:20, 1:200]
   
+  set.seed(5212)
   na.feats <- sample(1:ncol(X), 20)
   
   for (c in na.feats) {
@@ -34,6 +36,10 @@ test_that("tune.spca works with NA input", {
   
   expect_warning({object <- tune.spca(X,ncomp = 2, 
                                       folds = 5, 
-                                      test.keepX = grid.keepX, nrepeat = 3)},
+                                      test.keepX = grid.keepX, nrepeat = 3,
+                                      BPPARAM = SerialParam(RNGseed = 5212))},
                  "NAs present")
+  
+  expect_equal(object$choice.keepX[[1]], 15)
+  expect_equal(object$choice.keepX[[2]], 5)
 })
