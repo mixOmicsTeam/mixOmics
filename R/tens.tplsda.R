41bbf271a2e7a7107377f713ae68873de756a2db
BLuLaptop <brendannlu5@gmail.com>
2024-09-29 01:12:11 +1000

    update: completed tens.tplsda.R finnicky - allow specification of multiple observations of categorical label across time points, with potentially new classes introduced that were not present in previous time points

--- R/tens.tplsda.R
+++ R/tens.tplsda.R
@@ -3,18 +3,22 @@
 # on Kilmer's tensor m-product algebra
 # ==============================================================================
 
-#' Check if y input is a vector, matrix or tensor that can be inferred as
-#' classes across all time points, if so return these classes in consistent form
-#' as a vector.
+#' Check if y input is a vector, matrix or tensor that can be inferred as fixed
+#' class labels for each sample across all time points; if so return these
+#' classes in consistent form as a vector.
 #'
+#' @author Brendan Lu
 #' @keywords internal
 .y_to_vec <- function(y) {
+  # input is already a vector
   if (is.null(dim(y))) {
     return(y)
   }
+  # input is a matrix with a single column
   if (length(dim(y)) == 2 && dim(y)[2] != 1) {
     return(c(y))
   }
+  # input is a tensor with a single column
   if (length(dim(y)) == 3 && dim(y)[2] == 1 && dim(y)[3] != 1) {
     return(c(y))
   }
@@ -28,8 +32,31 @@
   ")
 }
 
-.y_to_tens <- function() {
-  NULL
+#' Check if y input is a matrix or tensor that can be inferred as repeated class
+#' measurements for each sample; if so, return these classes in a consistent
+#' form as a n x 1 x t tensor.
+#'
+#' Note: We do not have to check that the dimensions are compatible with x
+#' tensor input, this check will be done in the call to tpls.
+#'
+#' @author Brendan Lu
+#' @keywords internal
+.y_to_tens <- function(y) {
+  # input is a n x t matrix
+  if (length(dim(y)) == 2) {
+    n <- dim(y)[1]
+    t <- dim(y)[2]
+    return(array(y, dim = c(n, 1, t)))
+  }
+  # input is a n x 1 x t tensor
+  if (length(dim(y)) == 3 && dim(y)[2] == 1) {
+    return(y)
+  }
+  stop("
+    When `multilevel = TRUE` in tplsda, please ensure that y input is either a
+    n x t matrix, or n x 1 x t column tensor, representing the classes for
+    each sample across the t repeated measurements.
+  ")
 }
 
 #' Run tensor PLSDA-like analysis
@@ -54,25 +81,37 @@ tplsda <- function(
     stop("Please ensure x input tensor is an order-3 array")
   }
 
+  # BLTODO: may need to vendor unmap.R into big copy-paste script for Saritha
+  # or library(mixOmics) at the top would work neatly too
   if (multilevel) {
+    y_tens <- .y_to_tens(y)
+    n <- dim(y_tens)[1]
+    t <- dim(y_tens)[3]
 
+    # we need to determine all the unique levels that show up across all
+    # repeated observations for all samples
+    factors_list <- lapply(array(seq_len(t)), function(i) factor(y_tens[, , i]))
+    unique_levels <- unique(unlist(lapply(factors_list, levels)))
+
+    y <- array(0, dim = c(n, length(unique_levels), t))
+    for (i in seq_len(t)) {
+      # unmap conveniently allows us to manually control the total sets of
+      # groups (class measurements) our input sample is drawn from
+      y[, , i] <- unmap(factors_list[[i]], unique_levels)
+    }
   } else {
     y_vec <- .y_to_vec(y)
-    if (dim(x)[3] > 1) {
-      y_vec <- factor(y_vec)
-      if (nlevels(y_vec) == 1) {
-        stop("y should contain 2 or more distinct classes")
-      }
-
-      y_mat <- unmap(y_vec)
-      # BLTODO: WANT colnames(y_mat) = levels(y_vec), and be able to propogate
-      # names into tpls - need to work on preserving names for tpls and tpca
-      # when mixOmics2 is rolled out to end users
-
-      # if y specifies the same classes across all time points, fill y into a
-      # tensor to match the number of time points in t
-      y <- array(y_mat, dim = c(dim(y_mat), dim(x)[3]))
+    y_vec <- factor(y_vec)
+    if (nlevels(y_vec) == 1) {
+      stop("y should contain 2 or more distinct classes")
     }
+    y_mat <- unmap(y_vec)
+    # bltodo: WANT colnames(y_mat) = levels(y_vec), and be able to propogate
+    # names into tpls - need to work on preserving names for tpls and tpca
+    # when mixOmics2 is rolled out to end users (see existing plsda)
+
+    # fill y matrix into a tensor with the same t as x tensor input
+    y <- array(y_mat, dim = c(dim(y_mat), dim(x)[3]))
   }
 
   # note the rest of parameter validation will be done in tpls call
