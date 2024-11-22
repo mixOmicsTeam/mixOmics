48db6abec6c6ffbb0c3cf8b6ef654011496ab6d6
Eva Hamrud <50098063+evaham1@users.noreply.github.com>
2024-11-22 13:41:49 +1100

    update tune.splsda so runs perf if test.keepX=NULL, update docs and exampels

---
+++ examples/tune.splsda-examples.R
@@ -0,0 +1,46 @@
+## First example: analysis with sPLS-DA
+data(breast.tumors)
+X = breast.tumors$gene.exp
+Y = as.factor(breast.tumors$sample$treatment)
+
+# first tune on components only
+tune = tune.splsda(X, Y, ncomp = 5, logratio = "none",
+                   nrepeat = 10, folds = 10,
+                   test.keepX = NULL, 
+                   progressBar = TRUE,
+                   seed = 20) # set for reproducibility of example only
+plot(tune) # optimal distance = centroids.dist
+tune$choice.ncomp # optimal component number = 3
+
+# then tune optimal keepX for each component
+tune = tune.splsda(X, Y, ncomp = 3, logratio = "none",
+                   nrepeat = 10, folds = 10, 
+                   test.keepX = c(5, 10, 15), dist = "centroids.dist",
+                   progressBar = TRUE,
+                   seed = 20)
+
+plot(tune)
+tune$choice.keepX # optimal number of variables to keep c(15, 5, 15)
+
+\dontrun{
+tune = tune.splsda(X, Y, ncomp = 3, logratio = "none",
+                   nrepeat = 10, folds = 10, 
+                   test.keepX = c(5, 10, 15), already.tested.X = c(5, 10),
+                   dist = "centroids.dist",
+                   progressBar = TRUE,
+                   seed = 20)
+plot(tune)
+
+## Second example: multilevel one-factor analysis with sPLS-DA
+
+data(vac18)
+X = vac18$genes
+Y = vac18$stimulation
+# sample indicates the repeated measurements
+design = data.frame(sample = vac18$sample)
+
+tune = tune.splsda(X, Y = Y, ncomp = 3, nrepeat = 10, logratio = "none",
+                   test.keepX = c(5,50,100),folds = 10, dist = "max.dist", multilevel = design)
+
+plot(tune)
+}
