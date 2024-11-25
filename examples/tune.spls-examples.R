413de191ac18f3f44dbeb8f5fb5ac7e8e6413870
Eva Hamrud <50098063+evaham1@users.noreply.github.com>
2024-11-25 13:32:40 +1100

    update tune.spls examples to include spls1 model

--- examples/tune.spls-examples.R
+++ examples/tune.spls-examples.R
@@ -1,3 +1,5 @@
+## sPLS2 model (i.e. more than one Y outcome variable)
+
 # set up data
 data(liver.toxicity)
 X <- liver.toxicity$gene
@@ -16,3 +18,14 @@ tune.res <- tune.spls( X, Y, ncomp = 3,
                       test.keepY = c(3, 6, 8), measure = "cor",
                       folds = 5, nrepeat = 3, progressBar = TRUE)
 plot(tune.res) # plot outputs
+
+## sPLS1 model (i.e. only one Y outcome variable)
+
+# set up data 
+Y1 <- liver.toxicity$clinic[,1]
+
+# tune spls model for components only
+plot(tune.spls(X, Y1, ncomp = 3, folds = 3, test.keepX = NULL, test.keepY = NULL))
+
+# tune spls model for number of X variables to keep, note for sPLS1 models 'measure' needs to be set
+plot(tune.spls(X, Y1, ncomp = 3, folds = 3, test.keepX = c(5, 10, 15), test.keepY = c(3, 6, 8), measure = "MSE"))
