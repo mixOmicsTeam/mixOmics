3212cc0a3dc92bab8651d6203f6b9b4def298aa4
Eva Hamrud <50098063+evaham1@users.noreply.github.com>
2025-03-05 12:11:32 +1100

    unit tests mint.block.pls and mint.block.spls

---
+++ tests/testthat/test-mint.block.pls.R
@@ -0,0 +1,17 @@
+context("mint.block.pls")
+
+# Test
+test_that("mint.block.pls works", {
+  data(breast.TCGA)
+  study = c(rep("study1", 150), rep("study2", 70))
+  mrna = rbind(breast.TCGA$data.train$mrna, breast.TCGA$data.test$mrna)
+  mirna = rbind(breast.TCGA$data.train$mirna, breast.TCGA$data.test$mirna)
+  Y = mrna[, 1]
+  mrna = mrna[, -1]
+  data = list(mrna = mrna, mirna = mirna)
+  res = mint.block.plsda(data, Y, study=study, ncomp=2)
+  expect_equal(res$loadings$mrna[5], -0.08031856)
+  expect_equal(res$mode, "regression")
+})
+  
+               
