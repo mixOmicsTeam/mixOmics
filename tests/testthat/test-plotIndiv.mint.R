caa68a046a2615416804c871b21da232bbe113fb
Eva Hamrud <50098063+evaham1@users.noreply.github.com>
2025-03-19 11:41:23 +1100

    add plotIndiv.mint tests for mint plsda objs

--- tests/testthat/test-plotIndiv.mint.R
+++ tests/testthat/test-plotIndiv.mint.R
@@ -1,12 +1,14 @@
 context("plotIndiv.mint")
 
+#set up model
+data(stemcells)
+res = mint.splsda(X = stemcells$gene, Y = stemcells$celltype, ncomp = 2, keepX = c(10, 5),
+                  study = stemcells$study)
+
 ## ------------------------------------------------------------------------ ##
 ## Test that outputs are correct when running default style = "ggplot2"
 
 test_that("plotIndiv works for mint.(s)plsda", {
-  data(stemcells)
-  res = mint.splsda(X = stemcells$gene, Y = stemcells$celltype, ncomp = 2, keepX = c(10, 5),
-                    study = stemcells$study)
   pl.res <-   plotIndiv(res)
   # check coordinates
   .expect_numerically_close(pl.res$graph$data$x[1], -1.543685)
@@ -40,17 +42,95 @@ test_that("mint.splsda can be visualised with predicted background", {
 ## ------------------------------------------------------------------------ ##
 ## Plotting with 'lattice' style
 
+test_that("plotIndiv works for mint.(s)plsda - lattice", {
+  pl.res <-   plotIndiv(res, style = "lattice")
+  # check coordinates
+  .expect_numerically_close(pl.res$df$x[1], -1.543685)
+  # check correct output structure
+  expect_equal(names(pl.res), c("df", "graph"))
+  # check right number of samples
+  expect_equal(dim(res$X)[1], dim(pl.res$df)[1])
+})
 
 ## ------------------------------------------------------------------------ ##
 ## Plotting with 'graphics' style
 
+test_that("plotIndiv works for mint.(s)plsda - graphics", {
+  pl.res <-   plotIndiv(res, style = "graphics")
+  # check coordinates
+  .expect_numerically_close(pl.res$df$x[1], -1.543685)
+  # check correct output structure
+  expect_equal(names(pl.res), c("df", "graph"))
+  # check right number of samples
+  expect_equal(dim(res$X)[1], dim(pl.res$df)[1])
+})
 
 ## ------------------------------------------------------------------------ ##
-## Plotting with '3d' style
+## ------------------------------------------------------------------------ ##
+## vdiffr testing - "ggplot2" with MINT sPLS-DA object
+library(vdiffr)
 
-library(rgl)
+test_that("plotIndiv works for mint sPLSDA plotting studies separately and together", {
+  skip_on_ci() # only run the vdiffr tests locally
 
-# Clear the rgl device
-if (rgl::rgl.cur() > 0) {
-  rgl::close3d()
-}
+  invisible(capture.output(
+  # all studies in one plot
+  expect_doppelganger(
+    title = "mint sPLSDA plot global",
+    fig = plotIndiv(res, study = "global"))
+  ))
+  invisible(capture.output(
+  # studies in separate plots
+  expect_doppelganger(
+    title = "mint sPLSDA plot studies facetted",
+    fig = plotIndiv(res, study = "all.partial"))
+  ))
+  invisible(capture.output(
+  # studies in separate plots - different layout
+  expect_doppelganger(
+    title = "mint sPLSDA plot studies facetted different layout",
+    fig = plotIndiv(res, study = "all.partial", layout = c(3,4)))
+  ))
+})
+
+test_that("plotIndiv works for mint sPLSDA plotting controlled groups cols, ellipse, etc", {
+  skip_on_ci() # only run the vdiffr tests locally
+
+  invisible(capture.output(
+  # default groups custom cols
+  expect_doppelganger(
+    title = "mint sPLSDA plot custom cols default groups",
+    fig = plotIndiv(res, col = c("red", "yellow", "blue")))
+  ))
+  invisible(capture.output(
+  # default cols custom groups
+  expect_doppelganger(
+    title = "mint sPLSDA plot default cols custom groups",
+    fig = plotIndiv(res, group = as.factor(c(rep("A", 50), rep("B", 50), rep("C", 25))),
+    legend = TRUE))
+  ))
+  invisible(capture.output(
+  # ellipse
+  expect_doppelganger(
+    title = "mint sPLSDA plot with ellipse",
+    fig = plotIndiv(res, ellipse = TRUE, legend = TRUE))
+  ))
+  invisible(capture.output(
+  # star
+  expect_doppelganger(
+    title = "mint sPLSDA plot with star",
+    fig = plotIndiv(res, centroid = TRUE, star = TRUE, legend = TRUE))
+  ))
+
+})
+
+test_that("plotIndiv works for mint sPLSDA with background", {
+  skip_on_ci() # only run the vdiffr tests locally
+
+  background = background.predict(res, comp.predicted=2, dist = "max.dist")
+  invisible(capture.output(
+  expect_doppelganger(
+    title = "mint sPLSDA plot with background",
+    fig = plotIndiv(res, background = background, legend = TRUE))
+  ))
+})
